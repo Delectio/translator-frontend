85e39f9ef6fd7eaa0431504e45b80dd219153812
Emmanuel Osuh <mrmusonant@gmail.com>
2019-09-12 23:50:35 +0100

    Add css and sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,11 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.html$/,
